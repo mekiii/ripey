9a34c1c838d3cedb0c13df58b07f12c9681e43ab
Meki <Meki@Mekis-Mac.local>
2019-02-07 23:09:56 +0100

    Added interactive messages, dialog and final message

--- slack-bot/app.js
+++ slack-bot/app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 const fs = require('fs');
 var shuffleNumber = 1;
-
+var trigger_id;
 
 
 //Declare some variables
@@ -15,7 +15,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 //Get slackclient
 const { WebClient } = require('@slack/client');
 // An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
-const token = "xoxb-447711350823-540306733076-89uSk0O0p8EPmZh2UN0KPYKy";
+const token = "xoxb-447711350823-540306733076-pNY2IRPFirosJkwVZOonhlke";
 const web = new WebClient(token);
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
 const conversationId = 'CFXFT82NB';
@@ -60,6 +60,7 @@ const ripenessNotification = {
         ]
     }
 
+    //Read recipes
   var myRecipes;
   fs.readFile('./recipes.json', function read(err, data) {
     if (err) {
@@ -69,24 +70,43 @@ const ripenessNotification = {
     myRecipes = JSON.parse(content);
     console.log(myRecipes.length);
 });
-//console.log(myRecipes.length);
- /*
+
+//Read final message js
+var finalMessage;
+fs.readFile('./finalMessage.json', function read(err, data) {
+  if (err) {
+      throw err;
+  }
+  content = data.toString('utf8');
+  finalMessage = JSON.parse(content);
+});
+
+//Read dialog js
+var dialog;
+fs.readFile('./dialog.json', function read(err, data) {
+  if (err) {
+      throw err;
+  }
+  content = data.toString('utf8');
+  dialog = JSON.parse(content);
+});
+
+
+/*
 function loadJSON (jsonPath){
-  var myContent;
   fs.readFile(jsonPath , function read(err, data) {
     if (err) {
         throw err;
     }
     content = data.toString('utf8');
-    myContent = JSON.parse(content);
+    console.log("Bla : " + content);
+    return content;
   });
-  return myContent;
+  
 }
 var recipePath = './recipes.json';
 var myRecipes = loadJSON(recipePath);
-console.log(myRecipes);
 */
-
 
 //First post ripeness notification
 web.chat.postMessage({ channel: conversationId, text: ripenessNotification.text, attachments: ripenessNotification.attachments })
@@ -108,27 +128,59 @@ app.post('/', urlencodedParser, (req, res) =>{
   console.log("###########################################################");
   res.status(200).end() // best practice to respond with empty 200 status code
   var reqBody = req.body;
-  var responseURL = JSON.parse(reqBody.payload).response_url;
   var actions = JSON.parse(reqBody.payload).actions;
+  var trigger_id = JSON.parse(reqBody.payload).trigger_id;
+  var reqToken = JSON.parse(reqBody.payload).token;
+  var type = JSON.parse(reqBody.payload).type;
+  console.log(JSON.parse(reqBody.payload));
   var message;
+  var sendChatUpdate = false;
+  var openDialog = false;
 
-  switch(actions[0].value){
-    case "shuffle":
-    // Shuffle recipes
-    shuffleNumber = (shuffleNumber + 1) % myRecipes.length;
-    message = myRecipes[shuffleNumber];
-    break;
-    case "select":
-    break;
-    case "quit":
-    message = cancelMessage;
-  }
-  web.chat.update({ channel: conversationId, text: message.text, attachments: message.attachments, ts: timeStamp })
-  .then((res) => {
-    // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
+  if (actions){
+    switch(actions[0].value){
+            case "shuffle":
+            // Shuffle recipes
+            shuffleNumber = (shuffleNumber + 1) % myRecipes.length;
+            message = myRecipes[shuffleNumber];
+            sendChatUpdate = true;
+            break;
+            case "select":
+            openDialog = true;
+            break;
+            case "quit":
+            message = cancelMessage;
+            sendChatUpdate = true;
+            break;
+            case "zusammen":
+            message = cancelMessage;
+            sendChatUpdate = true;
+            break;
+            case "alleine":
+            message = cancelMessage;
+            sendChatUpdate = true;
+            break;
+        }
+    }
+
+    if (type == "dialog_submission"){
+        console.log("Hello, dialog submitted");
+        message = finalMessage;
+        sendChatUpdate = true;
+
+    }
+
+    if(sendChatUpdate) {
+        web.chat.update({ channel: conversationId, text: message.text, attachments: message.attachments, ts: timeStamp })
+        .then((res) => {
+            // `res` contains information about the posted message
+            console.log('Message sent: ', res.ts);
+        })
+        .catch(console.error);
+    }
+    if(openDialog){
+        web.dialog.open({reqToken, dialog, trigger_id});
+    }
 
 });
 
