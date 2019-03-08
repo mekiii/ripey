32a449a83eeb82a100dd484e38d79322e1ccd4f1
mekiii <mekong.lam@gmail.com>
2019-03-08 12:03:46 +0100

    Updated PHP-Interface

---
+++ 20190308_FromWebcamToDatabase_sm/database.php
@@ -0,0 +1,43 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "runmysql";
+$dbname = "planta";
+
+$datum = $_POST['Datum'];
+$pflanze = $_POST['Pflanze'];
+$status = $_POST['Status'];
+$ort = $_POST['Ort'];
+
+echo $datum;
+echo $pflanze;
+echo $status;
+echo $ort;
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+//CALL DATABASE
+
+// Attempt insert query execution
+$sql = "INSERT INTO anbau (Datum, Pflanze, Status, Ort) VALUES ('$datum', '$pflanze', '$status', '$ort')";
+if(mysqli_query($conn, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+ 
+// Close connection
+mysqli_close($conn);
+
+/*mysqli_close();*/
+
+
+?>
